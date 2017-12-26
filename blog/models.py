5920ad116a701109c897d773202e06f2a70004b3
Erkan Ercan <abajuradam@yahoo.com>
2017-12-26 03:27:34 +0300

    added blog comment model

--- blog/models.py
+++ blog/models.py
@@ -2,16 +2,32 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 
+
 class Post(models.Model):
-	yazar = models.ForeignKey('auth.User', on_delete=models.DO_NOTHING)
-	baslik = models.CharField(max_length=200)
-	yazi = models.TextField()
-	yaratilma_tarihi = models.DateTimeField(default=timezone.now)
-	yayinlanma_tarihi = models.DateTimeField(blank=True,null=True)
+    yazar = models.ForeignKey('auth.User', on_delete=models.DO_NOTHING)
+    baslik = models.CharField(max_length=200)
+    yazi = models.TextField()
+    yaratilma_tarihi = models.DateTimeField(default=timezone.now)
+    yayinlanma_tarihi = models.DateTimeField(blank=True, null=True)
 
-	def yayinla(self):
-		self.yayinlama_tarihi = timezone.now()
-		self.save()
+    def yayinla(self):
+        self.yayinlama_tarihi = timezone.now()
+        self.save()
 
-	def __str__(self):
-		return self.baslik
+    def __str__(self):
+        return self.baslik
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    yazar = models.CharField(max_length=200)
+    yazi = models.TextField
+    yaratilma_tarihi = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
