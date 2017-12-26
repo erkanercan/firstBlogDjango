a65dbfeb81bba07ae669bc7fce70d73631f3fd84
Erkan Ercan <abajuradam@yahoo.com>
2017-12-26 03:16:15 +0300

    urls.py

--- djangoProje/urls.py
+++ djangoProje/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include, url
-from blog import views
+from django.contrib.auth import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^accounts/login/$', views.login, name='login'),
